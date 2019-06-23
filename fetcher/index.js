7a38e3aa9ff7b7874b1bf6b5d7ba25cc9d1c53db
goFrendiAsgard <gofrendiasgard@gmail.com>
2019-06-23 10:40:16 +0700

    done, untested

--- fetcher/index.js
+++ fetcher/index.js
@@ -1,18 +1,29 @@
 // Fetch contants from environment variables
 const NATS_URL = process.env.NATS_URL
+const MONGO_URL = process.env.MONGO_URL
+const MONGO_DB = process.env.MONGO_DB
+
+const subscribeEvent = 'tasks.fetchAll';
+const dbName = 'fullstack'
 
 // Import libraries
 const nats = require('nats');
+const MongoClient = require('mongodb').MongoClient;
 
-const subscribeEvent = 'tasks.fetchAll';
 const nc = nats.connect(NATS_URL);
-nc.subscribe(subscribeEvent, (subscribeMsg) => {
+nc.subscribe(subscribeEvent, async (subscribeMsg) => {
     console.log(`consume from ${subscribeEvent}: ${subscribeEvent}`);
-    const {requestId, tasks} = JSON.parse(subscribeMsg);
+    const {requestId} = JSON.parse(subscribeMsg);
     const publishEvent = `${subscribeEvent}.${requestId}`
-    // TODO: do something with mongo here
+    // connect to mongo
+    const client = new MongoClient(MONGO_URL);
+    await client.connect();
+    const collection = client.db(MONGO_DB).collection('tasks');
+    // fetch tasks 
+    const tasks = await collection.find({}).toArray();
+    // publish tasks
     const publishMsg = JSON.stringify(tasks)
     console.log(`publishing to ${publishEvent}: ${publishMsg}`);
     nc.publish(publishEvent, publishMsg);
+    client.close();
 });
-
--- saver/index.js
+++ saver/index.js
@@ -1,20 +1,29 @@
 // Fetch contants from environment variables
 const NATS_URL = process.env.NATS_URL
-
-// Import libraries
-const nats = require('nats');
+const MONGO_URL = process.env.MONGO_URL
+const MONGO_DB = process.env.MONGO_DB
 
 const subscribeInsertEvent = 'tasks.insert';
 const subscribeUpdateEvent = 'tasks.update';
+const dbName = 'fullstack'
+
+// Import libraries
+const nats = require('nats');
+const MongoClient = require('mongodb').MongoClient;
 
 const nc = nats.connect(NATS_URL);
 
 // listen to insert event
-nc.subscribe(subscribeInsertEvent, (subscribeMsg) => {
-    console.log(`consume from ${subscribeEvent}: ${subscribeInsertEvent}`);
+nc.subscribe(subscribeInsertEvent, async (subscribeMsg) => {
+    console.log(`consume from ${subscribeInsertEvent}: ${subscribeMsg}`);
     const {requestId, tasks} = JSON.parse(subscribeMsg);
     const publishEvent = `${subscribeInsertEvent}.${requestId}`
-    // TODO: do something with mongo here
+    // connect to mongo
+    const client = new MongoClient(MONGO_URL);
+    await client.connect();
+    const collection = client.db(MONGO_DB).collection('tasks');
+    // insert tasks 
+    await collection.insertMany(tasks);
     const publishMsg = JSON.stringify(tasks)
     console.log(`publishing to ${publishEvent}: ${publishMsg}`);
     nc.publish(publishEvent, publishMsg);
@@ -22,11 +31,19 @@ nc.subscribe(subscribeInsertEvent, (subscribeMsg) => {
 
 
 // listen to update event
-nc.subscribe(subscribeUpdateEvent, (subscribeMsg) => {
-    console.log(`consume from ${subscribeEvent}: ${subscribeUpdateEvent}`);
+nc.subscribe(subscribeUpdateEvent, async (subscribeMsg) => {
+    console.log(`consume from ${subscribeUpdateEvent}: ${subscribeMsg}`);
     const {requestId, tasks} = JSON.parse(subscribeMsg);
     const publishEvent = `${subscribeUpdateEvent}.${requestId}`
-    // TODO: do something with mongo here
+    // connect to mongo
+    const client = new MongoClient(MONGO_URL);
+    await client.connect();
+    const collection = client.db(MONGO_DB).collection('tasks');
+    for(const singleTask of tasks) {
+        const {id, task, done} = singleTask;
+        await collection.updateOne({_id: id}, {$set: {task, done}});
+    }
+    // fetch tasks 
     const publishMsg = JSON.stringify(tasks)
     console.log(`publishing to ${publishEvent}: ${publishMsg}`);
     nc.publish(publishEvent, publishMsg);
