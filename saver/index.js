81d24ac5f6919e0be293595f2c452d3ee24b0426
goFrendiAsgard <gofrendiasgard@gmail.com>
2019-06-24 18:14:56 +0700

    backend done

--- saver/index.js
+++ saver/index.js
@@ -9,14 +9,15 @@ const dbName = 'fullstack'
 
 // Import libraries
 const nats = require('nats');
-const MongoClient = require('mongodb').MongoClient;
+const {ObjectId, MongoClient} = require('mongodb');
 
 const nc = nats.connect(NATS_URL);
 
 // listen to insert event
 nc.subscribe(subscribeInsertEvent, async (subscribeMsg) => {
     console.log(`consume from ${subscribeInsertEvent}: ${subscribeMsg}`);
-    const {requestId, tasks} = JSON.parse(subscribeMsg);
+    let {requestId, tasks} = JSON.parse(subscribeMsg);
+    tasks = preprocessTasks(tasks);
     const publishEvent = `${subscribeInsertEvent}.${requestId}`
     // connect to mongo
     const client = new MongoClient(MONGO_URL);
@@ -33,18 +34,27 @@ nc.subscribe(subscribeInsertEvent, async (subscribeMsg) => {
 // listen to update event
 nc.subscribe(subscribeUpdateEvent, async (subscribeMsg) => {
     console.log(`consume from ${subscribeUpdateEvent}: ${subscribeMsg}`);
-    const {requestId, tasks} = JSON.parse(subscribeMsg);
+    let {requestId, tasks} = JSON.parse(subscribeMsg);
+    tasks = preprocessTasks(tasks);
     const publishEvent = `${subscribeUpdateEvent}.${requestId}`
     // connect to mongo
     const client = new MongoClient(MONGO_URL);
     await client.connect();
     const collection = client.db(MONGO_DB).collection('tasks');
     for(const singleTask of tasks) {
-        const {id, task, done} = singleTask;
-        await collection.updateOne({_id: id}, {$set: {task, done}});
+        const {_id, task, done} = singleTask;
+        await collection.updateOne({_id: ObjectId(_id)}, {$set: {task, done}});
     }
     // fetch tasks 
     const publishMsg = JSON.stringify(tasks)
     console.log(`publishing to ${publishEvent}: ${publishMsg}`);
     nc.publish(publishEvent, publishMsg);
 });
+
+function preprocessTasks(tasks) {
+    return tasks.map((task) => {
+        task.task = task.task || '';
+        task.done = task.done || false;
+        return task;
+    });
+}
